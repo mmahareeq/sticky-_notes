34ec1fd11d8c53698fe92bf51145b7a3ef98cd97
mmahareeq <52622730+mmahareeq@users.noreply.github.com>
2019-07-17 18:22:52 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,54 @@
+
+		# include <iostream>
+		using namespace std;
+		int main()
+		{
+			// declaration of variables
+		int number ;
+
+		string username ;
+
+		cout << "Welcome to the brand new “Sticky Notes”!\n Here is the list of operation this program offers:\n1- Add new user\n2- Add new note\n3- View notes for a specific user\n4- Exit"<<endl;
+		
+		cout << "enter of number :" ;
+		
+		cin  >> number;
+		// choose a number from the main menu
+		switch (number)
+		{
+		case 1 :
+			
+			break;
+
+		case 2 :
+			 
+			break;
+
+		case  3 :
+
+			break ;
+	
+		case 4:
+			return 1;
+			break ;
+		
+		default:
+			;
+
+
+
+
+
+		}
+
+
+
+
+
+
+		 system("pause");
+
+		 return 0;
+
+		}
+
