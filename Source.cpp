5ba5d92522a1f6d3b2c4cecfa5f0255f95bb1c41
Rahmeh98 <50781072+Rahmeh98@users.noreply.github.com>
2019-07-17 23:37:22 +0300

    Main menu

--- Source.cpp
+++ Source.cpp
@@ -1,54 +1,67 @@
+/**********************************************
+Project Title: Sticky Notes
+This project is an application where multiple users can store their notes on the computer, then retrieve them.
+Done by : Rahmeh Tartouri && Mariam Mahariq
+17/07/2019
+***********************************************/
 
-		# include <iostream>
-		using namespace std;
-		int main()
-		{
-			// declaration of variables
-		int number ;
+#include<iostream>
+#include<string>
+#include<fstream>
+using namespace std;
+int main()
+{
+	int number;
+	string firstName ,  lastName , fileName;
+	ofstream UserFile;
 
-		string username ;
+	cout << " Welcome to the brand new “Sticky Notes”!\n Here is the list of operation this program offers:\n1- Add new user\n2- Add new note\n3- View notes for a specific user\n4- Exit "  <<  endl;
 
-		cout << "Welcome to the brand new “Sticky Notes”!\n Here is the list of operation this program offers:\n1- Add new user\n2- Add new note\n3- View notes for a specific user\n4- Exit"<<endl;
-		
-		cout << "enter of number :" ;
-		
-		cin  >> number;
-		// choose a number from the main menu
-		switch (number)
-		{
-		case 1 :
-			
-			break;
+	cout << " Enter the number : ";
+	cin >> number;
+	// choose a number from the main menu
+	switch (number)
+	     {
+	         case 1: 
+				    cout << " Welcome aboard new user! "  <<  endl  <<  " Please let me know your first name : ";
+				    cin >> firstName;
+					cout << " Great " << firstName << " , now please enter your last name : ";
+					cin >> lastName; 
+					cout << " Done! "  <<  endl;
+					cout << " Nice to meet you " << firstName << " " << lastName << endl << endl;
+					cout << " <Click Enter to return to main menu> " << endl;
+					fileName = firstName + "-" + lastName +  ".txt" ; //crate file name
+					UserFile.open ( fileName ); //open file using user name 
+				    break;
 
-		case 2 :
-			 
-			break;
+	         case 2:
 
-		case  3 :
+		            break;
 
-			break ;
-	
-		case 4:
-			return 1;
-			break ;
-		
-		default:
-			;
+	         case  3:
 
+		            break;
 
+	         case 4:
+		            return 1;
+		            break;
 
-
-
-		}
-
-
-
-
+	         default:
+		            ;
 
 
-		 system("pause");
 
-		 return 0;
 
-		}
 
+	     }
+
+
+
+
+
+
+
+	system("pause");
+	return 0;
+
+}
