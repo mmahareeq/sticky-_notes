83448aa539bac247fbafcf46635067cb08432c71
mmahareeq <52622730+mmahareeq@users.noreply.github.com>
2019-07-20 14:12:46 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,54 +1,92 @@
 
-		# include <iostream>
-		using namespace std;
-		int main()
-		{
-			// declaration of variables
-		int number ;
+		/***********************************************
+Project Title: Sticky Notes
+This project is an application where multiple users can store their notes on the computer, then retrieve them.
+Done by : Rahmeh Tartouri && Mariam Mahariq
+17/07/2019
+************************************************/
 
-		string username ;
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<ctime>
+using namespace std;
+int main()
+{
+	int number;
+	string firstName ,  lastName , fileName , outdata ;
+	
+	ifstream userfile;
+	
+	bool rest = true ;
+	while(rest)
+	{	
+		cout << " Welcome to the brand new â€œSticky Notesâ€!\n Here is the list of operation this program offers:\n1- Add new user\n2- Add new note\n3- View notes for a specific user\n4- Exit "  <<  endl;
 
-		cout << "Welcome to the brand new “Sticky Notes”!\n Here is the list of operation this program offers:\n1- Add new user\n2- Add new note\n3- View notes for a specific user\n4- Exit"<<endl;
-		
-		cout << "enter of number :" ;
-		
-		cin  >> number;
+		cout << " Enter the number : ";
+		cin >> number;
 		// choose a number from the main menu
 		switch (number)
-		{
-		case 1 :
-			
-			break;
+			 {
+				 case 1: 
+					
+					break;
+				case 2:
+					  
 
-		case 2 :
-			 
-			break;
+					break;
 
-		case  3 :
+				 case  3:
+				 
+					 cout << "Retrieve your notes? Absolutely!"  << endl << "Please let know your  full name first: ";
+					 cin.get();
+                                          getline(cin,fileName);
+					 fileName =  fileName + ".txt" ;
+					 userfile.open(fileName.c_str());  // open file                   
+					 if (userfile.fail()) // Check if file exists
 
-			break ;
-	
-		case 4:
-			return 1;
-			break ;
-		
-		default:
-			;
+						 {
+							cout <<" Umm, canâ€™t find any saved notes for you. "<< endl;
+						 }
+					 else
+						{
+						 cout << "Found it!"<<endl;
+						 cout << " Here are your stored notes: " << endl << " ------------------------ " << endl;
+						 while(!userfile.eof())
+						 { 
+
+						 getline( userfile , outdata);
+						 cout <<  endl <<outdata << endl;
+						 }
+						 cout <<" ------------------------  "<< endl;
+						 cout << " Happy to serve you :) " << endl;
+
+
+						 }		
+
+					 break;
+
+				 case 4:
+					 return 1;
+
+					 break;
+					
+				 default:
+						;
 
 
 
 
 
-		}
+			 }
+
+
+	}
 
 
 
 
+	system("pause");
+	return 0;
 
-
-		 system("pause");
-
-		 return 0;
-
-		}
-
+}
