6a535cb03c5ac3f8cc46d513b84e303c6e6ec163
mmahareeq <52622730+mmahareeq@users.noreply.github.com>
2019-07-20 14:06:49 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -23,18 +23,58 @@ int main()
 	switch (number)
 	     {
 	         case 1: 
-		        cout << " Welcome aboard new user! "  <<  endl  <<  " Please let me know your first name : ";
-		        cin >> firstName;
-		        cout << " Great " << firstName << " , now please enter your last name : ";
-			cin >> lastName; 
-			cout << " Done! "  <<  endl;
-			cout << " Nice to meet you " << firstName << " " << lastName << endl << endl;
-			cout << " <Click Enter to return to main menu> " << endl;
-			fileName = firstName + "-" + lastName +  ".txt" ; //crate file name
-			UserFile.open ( fileName ); //open file using user name 
+		               cout << " Welcome aboard new user! "  <<  endl  <<  " Please let me know your first name : ";
+			       cin >> firstName;
+				cout << " Great " << firstName << " , now please enter your last name : ";
+				cin >> lastName; 
+				cout << " Done! "  <<  endl;
+				cout << " Nice to meet you " << firstName << " " << lastName << endl << endl;
+				cout << " <Click Enter to return to main menu> " << endl;
+				fileName = firstName + " " + lastName +  ".txt" ; //crate file name
+				userFile.open ( fileName ); //open file using user name 
 		        break;
 
 	         case 2:
+
+                                   cout << " Let’s add a new note ... " << endl;
+					cout << " Please enter your full name first : ";
+					cin.get();
+                                        getline(cin,fileName);
+					fileName = fileName + ".txt" ;
+					userFile.open(fileName , ios::app);
+					if (!userFile.fail())
+					   {
+						cout << " Your record is found, I’m now opening your file …. " << endl;
+						cout << " Ready! " << endl;
+						cout << " Please enter your note: " << endl;
+						// Read a line from standard input in str 
+					     
+						 cin.get(); 
+						
+						getline(cin, note);
+
+						// Applying time() 
+						time(&tt);
+
+						// Using localtime() 
+						ti = localtime(&tt);
+
+						/*cout << "Current Day, Date and Time is = "
+							<< asctime(ti);*/
+						userFile <<asctime(ti)<< note << endl ;
+						userFile.close();
+
+
+					   }
+					else
+					   {
+
+						cout << " Oh! Sorry the user name was not found, please check the name again and if this is your first time here, please go ahead and create a new user from the main menu ... " << endl;
+						
+
+					   }
+
+				    
 
 		        break;
 
